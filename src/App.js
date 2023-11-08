86aa1216a18a866d702b814ff11a8d4015c23748
Lance Hudson <lancexhudson@gmail.com>
2023-11-08 06:25:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Retirement Calculator</h1>
+      <h2>You can retire at age </h2>
+      <form action="">
+        <div>Target Retirement Amount</div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
